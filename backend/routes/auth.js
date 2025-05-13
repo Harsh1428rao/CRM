4467f9b4348e760296b98969a831cab18e8d22bd
Harsh Rao <hrao1428@gmail.com>
2025-05-13 19:03:26 +0530

    commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://crm-1-30zn.onrender.com//api/auth/google/callback"
+    callbackURL: "https://crm-1-30zn.onrender.com/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
